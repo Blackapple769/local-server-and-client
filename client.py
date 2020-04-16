7c6462f12467bbc849b6b119730cb0ec2e7ac348
Blackapple769 <53877690+Blackapple769@users.noreply.github.com>
2020-04-16 16:46:10 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "DISCONNECTED!"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+connected = True
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+while connected:
+    s = input("Send: ")
+    if s == "QUIT.":
+        connected = False
+    else:
+
+        send(s)
+
+else:
+    send(DISCONNECT_MESSAGE)
